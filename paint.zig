02843e7e9171fbe3a1d1060347cc5303a2adce8e
Nick Erdmann <n@nirf.de>
2019-08-04 18:48:05 +0200

    cleanup and disable watchdog timer

--- paint.zig
+++ paint.zig
@@ -10,15 +10,10 @@ pub fn main() void {
     var pointer: *AbsolutePointerProtocol = undefined;
     var graphics: *GraphicsOutputProtocol = undefined;
     var pointer_state = AbsolutePointerState.init();
-    var color = [1]GraphicsOutputBltPixel{GraphicsOutputBltPixel{
-        .blue = 0,
-        .green = 0,
-        .red = 0,
-        .reserved = 0,
-    }};
-    const rainbow = [16]GraphicsOutputBltPixel{
+    var selected: u8 = 0;
+    const colors = [16]GraphicsOutputBltPixel{
         GraphicsOutputBltPixel{ .blue = 0x00, .green = 0x00, .red = 0x00, .reserved = 0 },
-        GraphicsOutputBltPixel{ .blue = 0x00, .green = 0x00, .red = 0xaa, .reserved = 0 },
+        GraphicsOutputBltPixel{ .blue = 0xaa, .green = 0x00, .red = 0x00, .reserved = 0 },
         GraphicsOutputBltPixel{ .blue = 0x00, .green = 0xaa, .red = 0x00, .reserved = 0 },
         GraphicsOutputBltPixel{ .blue = 0xaa, .green = 0xaa, .red = 0x00, .reserved = 0 },
         GraphicsOutputBltPixel{ .blue = 0x00, .green = 0x00, .red = 0xaa, .reserved = 0 },
@@ -35,23 +30,34 @@ pub fn main() void {
         GraphicsOutputBltPixel{ .blue = 0xff, .green = 0xff, .red = 0xff, .reserved = 0 },
     };
 
+    // Disable watchdog
+    _ = boot_services.setWatchdogTimer(0, 0, 0, null);
+
+    // Set pointers to protocols
     _ = boot_services.locateProtocol(&AbsolutePointerProtocol.guid, null, @ptrCast(*?*c_void, &pointer));
     _ = boot_services.locateProtocol(&GraphicsOutputProtocol.guid, null, @ptrCast(*?*c_void, &graphics));
 
+    // Draw color picker in the uppermost 16th of screen
     var i: u8 = 0;
     while (i < 16) : (i += 1) {
-        var c = [1]GraphicsOutputBltPixel{rainbow[i]};
+        var c = [1]GraphicsOutputBltPixel{colors[i]};
         _ = graphics.blt(&c, GraphicsOutputBltOperation.BltVideoFill, 0, 0, i * graphics.mode.info.horizontal_resolution / 16, 0, graphics.mode.info.horizontal_resolution / 16, graphics.mode.info.vertical_resolution / 16, 0);
     }
 
     while (true) {
+        // If pointer state has changed
         if (pointer.getState(&pointer_state) == 0) {
+            // If the color picker has been touched
             if (graphics.mode.info.vertical_resolution * pointer_state.current_y / (pointer.mode.absolute_max_y - pointer.mode.absolute_min_y) < graphics.mode.info.vertical_resolution / 16) {
-                color[0] = rainbow[16 * pointer_state.current_x / (pointer.mode.absolute_max_x - pointer.mode.absolute_min_x + 1)];
+                // Set selected color
+                selected = @intCast(u8, 16 * pointer_state.current_x / (pointer.mode.absolute_max_x - pointer.mode.absolute_min_x + 1));
             } else if (graphics.mode.info.horizontal_resolution * pointer_state.current_x / (pointer.mode.absolute_max_x - pointer.mode.absolute_min_x) >= 2 and graphics.mode.info.horizontal_resolution * pointer_state.current_x / (pointer.mode.absolute_max_x - pointer.mode.absolute_min_x) < graphics.mode.info.horizontal_resolution - 2 and graphics.mode.info.vertical_resolution * pointer_state.current_y / (pointer.mode.absolute_max_y - pointer.mode.absolute_min_y) < graphics.mode.info.vertical_resolution - 2) {
-                _ = graphics.blt(&color, GraphicsOutputBltOperation.BltVideoFill, 0, 0, graphics.mode.info.horizontal_resolution * pointer_state.current_x / (pointer.mode.absolute_max_x - pointer.mode.absolute_min_x) - 2, graphics.mode.info.vertical_resolution * pointer_state.current_y / (pointer.mode.absolute_max_y - pointer.mode.absolute_min_y) - 2, 5, 5, 0);
+                // Else draw
+                var c = [1]GraphicsOutputBltPixel{colors[selected]};
+                _ = graphics.blt(&c, GraphicsOutputBltOperation.BltVideoFill, 0, 0, graphics.mode.info.horizontal_resolution * pointer_state.current_x / (pointer.mode.absolute_max_x - pointer.mode.absolute_min_x) - 2, graphics.mode.info.vertical_resolution * pointer_state.current_y / (pointer.mode.absolute_max_y - pointer.mode.absolute_min_y) - 2, 5, 5, 0);
             }
         }
+        // wait 10 ms
         _ = boot_services.stall(10 * 1000);
     }
 }
