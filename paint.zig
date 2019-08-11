d547024106f7edbe29e6b4a97697f2eb55c566ea
Nick Erdmann <n@nirf.de>
2019-08-11 15:47:52 +0200

    integer improvements

--- paint.zig
+++ paint.zig
@@ -10,7 +10,7 @@ pub fn main() void {
     var pointer: *AbsolutePointerProtocol = undefined;
     var graphics: *GraphicsOutputProtocol = undefined;
     var pointer_state = AbsolutePointerState{};
-    var selected: u8 = 0;
+    var selected: u4 = 0;
     const colors = [16]GraphicsOutputBltPixel{
         GraphicsOutputBltPixel{ .blue = 0x00, .green = 0x00, .red = 0x00, .reserved = 0 },
         GraphicsOutputBltPixel{ .blue = 0xaa, .green = 0x00, .red = 0x00, .reserved = 0 },
@@ -38,8 +38,8 @@ pub fn main() void {
     _ = boot_services.locateProtocol(&GraphicsOutputProtocol.guid, null, @ptrCast(*?*c_void, &graphics));
 
     // Draw color picker in the uppermost 16th of screen
-    var i: u8 = 0;
-    while (i < 16) : (i += 1) {
+    comptime var i = 0;
+    inline while (i < 16) : (i += 1) {
         var c = [1]GraphicsOutputBltPixel{colors[i]};
         _ = graphics.blt(&c, GraphicsOutputBltOperation.BltVideoFill, 0, 0, i * graphics.mode.info.horizontal_resolution / 16, 0, graphics.mode.info.horizontal_resolution / 16, graphics.mode.info.vertical_resolution / 16, 0);
     }
@@ -49,7 +49,7 @@ pub fn main() void {
         _ = pointer.getState(&pointer_state);
         if (graphics.mode.info.vertical_resolution * pointer_state.current_y / (pointer.mode.absolute_max_y - pointer.mode.absolute_min_y) < graphics.mode.info.vertical_resolution / 16) {
             // If the color picker has been touched, set selected color
-            selected = @intCast(u8, 16 * pointer_state.current_x / (pointer.mode.absolute_max_x - pointer.mode.absolute_min_x + 1));
+            selected = @truncate(u4, 16 * pointer_state.current_x / (pointer.mode.absolute_max_x - pointer.mode.absolute_min_x + 1));
         } else if (graphics.mode.info.horizontal_resolution * pointer_state.current_x / (pointer.mode.absolute_max_x - pointer.mode.absolute_min_x) >= 2 and graphics.mode.info.horizontal_resolution * pointer_state.current_x / (pointer.mode.absolute_max_x - pointer.mode.absolute_min_x) < graphics.mode.info.horizontal_resolution - 2 and graphics.mode.info.vertical_resolution * pointer_state.current_y / (pointer.mode.absolute_max_y - pointer.mode.absolute_min_y) < graphics.mode.info.vertical_resolution - 2) {
             // Else if touch was at least 2 pixels into the drawing area, draw
             var c = [1]GraphicsOutputBltPixel{colors[selected]};
